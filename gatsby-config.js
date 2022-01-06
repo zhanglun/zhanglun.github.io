3d41f1c6b08a07050e32003b8739b0b2c6bf09a5
zhanglun <zhanglun1410@gmail.com>
2022-01-06 22:51:23 +0800

    feat: add gatsby-remark-images-anywhere

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,6 +69,9 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
+          {
+            resolve: `gatsby-remark-images-anywhere`,
+          },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,17 +1,17 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
-const { start } = require("repl")
+const path = require(`path`);
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const { start } = require("repl");
 const POSTSTATUS = {
   PUBLISH: "publish",
   WORKING: "working",
-}
+};
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
-  const { createPage } = actions
+  const { createPage } = actions;
 
   // Define a template for blog post
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  const blogList = path.resolve("./src/templates/blog-list.js")
+  const blogPost = path.resolve(`./src/templates/blog-post.js`);
+  const blogList = path.resolve("./src/templates/blog-list.js");
 
   // Get all markdown blog posts sorted by date
   const result = await graphql(
@@ -38,17 +38,17 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         }
       }
     `
-  )
+  );
 
   if (result.errors) {
     reporter.panicOnBuild(
       `There was an error loading your blog posts`,
       result.errors
-    )
-    return
+    );
+    return;
   }
 
-  const posts = result.data.postsRemark.nodes
+  const posts = result.data.postsRemark.nodes;
 
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
@@ -56,8 +56,9 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   if (posts.length > 0) {
     posts.forEach((post, index) => {
-      const previousPostId = index === 0 ? null : posts[index - 1].id
-      const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
+      const previousPostId = index === 0 ? null : posts[index - 1].id;
+      const nextPostId =
+        index === posts.length - 1 ? null : posts[index + 1].id;
 
       createPage({
         path: post.fields.slug,
@@ -67,13 +68,13 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           previousPostId,
           nextPostId,
         },
-      })
-    })
+      });
+    });
   }
 
   // create blog list pages
-  const postsPerPage = 5
-  const numPages = Math.ceil(posts.length / postsPerPage)
+  const postsPerPage = 5;
+  const numPages = Math.ceil(posts.length / postsPerPage);
 
   Array.from({ length: numPages }).forEach((_, i) => {
     createPage({
@@ -85,14 +86,14 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         numPages,
         currentPage: i + 1,
       },
-    })
-  })
+    });
+  });
 
   // 创建分类页面
-  const categoryTemplate = path.resolve("./src/templates/categories.js")
-  const categories = result.data.categoryGroup.group
+  const categoryTemplate = path.resolve("./src/templates/categories.js");
+  const categories = result.data.categoryGroup.group;
 
-  categories.forEach(category => {
+  categories.forEach((category) => {
     createPage({
       path: `/categories/${category.fieldValue}`,
       component: categoryTemplate,
@@ -100,50 +101,50 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
         category: category.fieldValue,
         count: category.totalCount,
       },
-    })
-  })
-}
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    let value = ""
+    let value = "";
 
     try {
       value = `/blogs${createFilePath({
         node,
         getNode,
-      })}`
+      })}`;
     } catch (err) {
-      value = value || `/blogs/${node.frontmatter.title}`
+      value = value || `/blogs/${node.frontmatter.title}`;
     }
 
     createNodeField({
       name: `slug`,
       node,
       value: value.trim(),
-    })
+    });
 
     if (node.frontmatter.date) {
       const rawDate = node.frontmatter.date.start || node.frontmatter.date;
-      const date = new Date(rawDate)
+      const date = new Date(rawDate);
 
-      const year = date.getFullYear()
-      const month = date.getMonth() + 1
-      const yearMonth = `${year}-${month}`
-      const day = date.getDate()
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      const yearMonth = `${year}-${month}`;
+      const day = date.getDate();
 
-      createNodeField({ node, name: "year", value: year })
-      createNodeField({ node, name: "month", value: month })
-      createNodeField({ node, name: "year-month", value: yearMonth })
-      createNodeField({ node, name: "day", value: day })
+      createNodeField({ node, name: "year", value: year });
+      createNodeField({ node, name: "month", value: month });
+      createNodeField({ node, name: "year-month", value: yearMonth });
+      createNodeField({ node, name: "day", value: day });
     }
   }
-}
+};
 
 exports.createSchemaCustomization = ({ actions, schema }) => {
-  const { createTypes } = actions
+  const { createTypes } = actions;
 
   // Explicitly define the siteMetadata {} object
   // This way those will always be defined even if removed from gatsby-config.js
@@ -166,7 +167,7 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
     type Social {
       twitter: String
     }
-  `)
+  `);
 
   const typeDefs = [
     "type MarkdownRemark implements Node { frontmatter: Frontmatter, fields: Fields }",
@@ -193,64 +194,64 @@ exports.createSchemaCustomization = ({ actions, schema }) => {
             dateformat: {},
           },
           resolve(source) {
-            const { date } = source
+            const { date } = source;
 
             if (date && date.start) {
-              return date.start
+              return date.start;
             }
 
-            return date
+            return date;
           },
         },
         tags: {
           type: "[String]",
           resolve(source) {
-            const { tags = [] } = source
+            const { tags = [] } = source;
 
             if (Array.isArray(tags)) {
-              return (tags || []).map(item => item.name || item)
+              return (tags || []).map((item) => item.name || item);
             }
 
             if (tags instanceof Object) {
-              return [tags.name]
+              return [tags.name];
             }
 
-            return [tags]
+            return [tags];
           },
         },
         categories: {
           type: "[String]",
           resolve(source) {
-            const { categories = [] } = source
+            const { categories = [] } = source;
 
             if (Array.isArray(categories)) {
-              return (categories || []).map(item => item.name || item)
+              return (categories || []).map((item) => item.name || item);
             }
 
             if (categories instanceof Object) {
-              return [categories.name]
+              return [categories.name];
             }
 
-            return [categories]
+            return [categories];
           },
         },
         status: {
           type: "String",
           resolve(source) {
             if (source.status && source.status.name) {
-              return source.status.name
+              return source.status.name;
             }
 
             if (source.draft) {
-              return POSTSTATUS.WORKING
+              return POSTSTATUS.WORKING;
             }
 
-            return POSTSTATUS.PUBLISH
+            return POSTSTATUS.PUBLISH;
           },
         },
       },
     }),
-  ]
+  ];
 
-  createTypes(typeDefs)
-}
+  createTypes(typeDefs);
+};
