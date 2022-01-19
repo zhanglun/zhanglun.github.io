95499e2e650275fe5365a3cc084cc418aa61421b
zhanglun <zhanglun1410@gmail.com>
2022-01-19 23:48:27 +0800

    improvement: add About Page and update some styles

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,43 @@
 import * as React from "react"
+import { Link, graphql } from "gatsby"
 
-export default function Component () {
-  return "Hello world"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const About = ({ data, location }) => {
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const siteMenu = data.site.siteMetadata?.menu || []
+  const description = data.site.siteMetadata?.description || ""
+
+  return (
+    <Layout
+      location={location}
+      title={siteTitle}
+      menu={siteMenu}
+      description={description}
+    >
+      <Seo title="All posts" />
+      <div className="main">
+        <h2>Hello World!</h2>
+      </div>
+    </Layout>
+  )
 }
+
+export default About;
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        menu {
+          id
+          name
+          url
+        }
+      }
+    }
+  }
+`
