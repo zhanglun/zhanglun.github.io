0add234b7ab1adb8b2f908be2dd2ca1b31fa0ad9
zhanglun <zhanglun1410@gmail.com>
2022-04-30 11:05:38 +0800

    style: add Table css for post

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `张小伦的网络日志`,
+    title: '张小伦的网络日志',
     author: {
-      name: `zhanglun`,
-      summary: `感受生活`,
+      name: 'zhanglun',
+      summary: '感受生活',
     },
-    description: `欢迎来到张小伦的网络日志 
-		一个记录生活，分享心得的博客`,
-    siteUrl: `https://zhanglun.github.io`,
+    description: '欢迎来到张小伦的网络日志 \n		一个记录生活，分享心得的博客',
+    siteUrl: 'https://zhanglun.github.io',
     social: {
-      twitter: `zhanglun1410`,
+      twitter: 'zhanglun1410',
     },
     menu: [
       {
@@ -36,80 +35,80 @@ module.exports = {
         id: 'about',
         name: '关于我',
         url: '/about',
-      }
+      },
     ],
   },
   plugins: [
-    `gatsby-plugin-image`,
+    'gatsby-plugin-image',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/blog`,
-        name: `blog`,
+        name: 'blog',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/content/notion`,
-        name: `notion`,
+        name: 'notion',
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
     {
-      resolve: `gatsby-transformer-remark`,
+      resolve: 'gatsby-transformer-remark',
       options: {
         gfm: true,
         plugins: [
           {
-            resolve: `gatsby-remark-classes`,
+            resolve: 'gatsby-remark-classes',
             options: {
               classMap: {
-                "heading[depth=1]": "title",
-                "heading[depth=2]": "subtitle",
-                paragraph: "para",
-                "link": "link-underline",
-                blockquote: "quote"
-              }
-            }
+                'heading[depth=1]': 'title',
+                'heading[depth=2]': 'subtitle',
+                paragraph: 'para',
+                link: 'link-underline',
+                blockquote: 'quote',
+              },
+            },
           },
           {
-            resolve: `gatsby-remark-images`,
+            resolve: 'gatsby-remark-images',
             options: {
               maxWidth: 630,
             },
           },
-           {
-             resolve: `gatsby-remark-responsive-iframe`,
-             options: {
-               wrapperStyle: `margin-bottom: 1.0725rem`,
-             },
-           },
           {
-            resolve: `gatsby-remark-images-anywhere`,
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: {
+              wrapperStyle: 'margin-bottom: 1.0725rem',
+            },
           },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
+          {
+            resolve: 'gatsby-remark-images-anywhere',
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
         ],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: `UA-50406624-2`,
+        trackingId: 'UA-50406624-2',
       },
     },
     {
-      resolve: `gatsby-plugin-feed`,
+      resolve: 'gatsby-plugin-feed',
       options: {
         query: `
           {
@@ -125,17 +124,14 @@ module.exports = {
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.nodes.map(node => {
-                return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
-                  date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
-                })
-              })
-            },
+            serialize: ({ query: { site, allMarkdownRemark } }) => allMarkdownRemark.nodes.map((node) => ({
+              ...node.frontmatter,
+              description: node.excerpt,
+              date: node.frontmatter.date,
+              url: site.siteMetadata.siteUrl + node.fields.slug,
+              guid: site.siteMetadata.siteUrl + node.fields.slug,
+              custom_elements: [{ 'content:encoded': node.html }],
+            })),
             query: `
               {
                 allMarkdownRemark(
@@ -155,30 +151,30 @@ module.exports = {
                 }
               }
             `,
-            output: "/rss.xml",
-            title: "张小伦的网络日志",
+            output: '/rss.xml',
+            title: '张小伦的网络日志',
           },
         ],
       },
     },
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `张小伦的网络日志`,
-        short_name: `张小伦`,
-        description: `欢迎来到张小伦的网络日志一个记录生活，分享心得的博客`,
-        start_url: `/`,
-        background_color: `#ffffff`,
+        name: '张小伦的网络日志',
+        short_name: '张小伦',
+        description: '欢迎来到张小伦的网络日志一个记录生活，分享心得的博客',
+        start_url: '/',
+        background_color: '#ffffff',
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        display: 'minimal-ui',
+        icon: 'src/images/icon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
