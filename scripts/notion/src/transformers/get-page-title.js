06dfaf3916c67224f9df7467fc67d960f2f5b990
zhanglun <zhanglun1410@gmail.com>
2022-04-30 11:01:10 +0800

    style: :rotating_light: script eslint fix

--- scripts/notion/src/transformers/get-page-title.js
+++ scripts/notion/src/transformers/get-page-title.js
@@ -1,9 +1,9 @@
-const { blockToString } = require("../block-to-string")
+const { blockToString } = require('../block-to-string');
 
 exports.getNotionPageTitle = (page) => {
-	const titleProperty = Object.keys(page.properties).find(
-		(key) => page.properties[key].type == "title",
-	)
+  const titleProperty = Object.keys(page.properties).find(
+    (key) => page.properties[key].type === 'title',
+  );
 
-	return blockToString(page.properties[titleProperty].title)
-}
+  return blockToString(page.properties[titleProperty].title);
+};
