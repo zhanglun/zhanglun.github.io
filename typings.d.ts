baf742d975b49de37d83a039459a51af8ff08d2b
zhanglun <zhanglun1410@gmail.com>
2022-06-11 10:24:47 +0800

    chore: add tsconfig; fix React no-use-before-define

--- typings.d.ts
+++ typings.d.ts
@@ -1,7 +1,1 @@
-declare module '*.css' {
-  interface IClassNames {
-    [className: string]: string
-  }
-  const classNames: IClassNames;
-  export = classNames;
-}
+declare module '*.module.css';
