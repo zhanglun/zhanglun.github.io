1d8aa414a50d39f9088d4d42fdd5ef13444f164d
zhanglun <zhanglun1410@gmail.com>
2022-07-31 17:20:42 +0800

    improvement: :lipstick: add theme for code block

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,9 +102,16 @@ module.exports = {
               wrapperStyle: 'margin-bottom: 1.0725rem',
             },
           },
-          'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
+          {
+            resolve: 'gatsby-remark-highlight-code',
+            options: {
+              lineNumbers: true,
+              editable: true,
+              theme: 'one-dark',
+            },
+          },
         ],
       },
     },
