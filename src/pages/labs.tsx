77be8288ad55771ea3165687a697c94d1c8c3171
zhanglun <zhanglun1410@gmail.com>
2022-10-04 04:23:13 +0000

    feat: add projects into Labs

--- src/pages/labs.tsx
+++ src/pages/labs.tsx
@@ -10,6 +10,35 @@ function About({ data, location }) {
   const siteMenu = data.site.siteMetadata?.menu || [];
   const description = data.site.siteMetadata?.description || '';
 
+  const projects = [
+    {
+      name: 'Lettura',
+      description: 'RSS 桌面阅读器',
+      url: 'https://github.com/zhanglun/lettura',
+      thumbnail: '',
+    },
+    {
+      name: 'Gatsby blog theme',
+      description: 'Person blog theme for Gatsby.js',
+      url: 'https://github.com/zhanglun/gatsby-starter-blog',
+    },
+    {
+      name: 'notify',
+      description: 'Notify Component',
+      url: 'https://github.com/zhanglun/notify',
+    },
+    {
+      name: 'hexo-theme-Tinny',
+      description: 'A theme for Hexo',
+      url: 'https://github.com/zhanglun/hexo-theme-Tinny',
+    },
+    {
+      name: 'bluerobin',
+      description: 'online todo list based on Vue ecosystem',
+      url: 'https://github.com/zhanglun/bluerobin',
+    },
+  ];
+
   return (
     <Layout
       location={location}
@@ -18,8 +47,18 @@ function About({ data, location }) {
       description={description}
     >
       <Seo title="张伦 实验室" />
-      <div>
+      <div className="lab-page">
         <PageHero title="实验室" subTitle="各种奇思妙想的实验" />
+        <div>
+          {
+          projects.map((project) => (
+            <div>
+              <p>{project.name}</p>
+              <p>{project.description}</p>
+            </div>
+          ))
+        }
+        </div>
       </div>
     </Layout>
   );
