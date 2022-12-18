b3685dbc46527d4454b6a0da6cae757a4946f725
zhanglun <zhanglun1410@gmail.com>
2022-12-18 18:16:18 +0800

    feat: Labs

---
+++ src/components/ProjectCards/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import * as style from './index.module.css';
+
+export interface ProjectItemProps {
+  name: string;
+  description: string;
+  url: string;
+}
+
+export interface ProjectProps {
+  list: ProjectItemProps[];
+}
+
+export const ProjectCards = (props: ProjectProps) => {
+  const { list } = props;
+  return (
+    <div className={style.list}>
+      {list.map((item, idx) => (
+        <div className={style.item}>
+          <div className={[style.image, style[`project${idx + 1}`]].join(' ')} />
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
