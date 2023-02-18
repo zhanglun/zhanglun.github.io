bc907f6b8c62230add45d1d340929abe8ac61ea1
zhanglun <zhanglun1410@gmail.com>
2023-02-18 19:30:12 +0800

    improvement: add svg

--- src/components/Home/Rich.tsx
+++ src/components/Home/Rich.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import type { MarkdownInstance } from "astro";
 import type { Frontmatter } from "src/types";
-import Card from '@components/Card';
 import slugify from "@utils/slugify";
 import Icon from "../../assets/icon.png";
+import Scene from "../../assets/blob-scene-haikei.svg";
+import Steps from "../../assets/layered-steps-haikei.svg";
+import Waves from "../../assets/layered-waves-haikei.svg";
 import { CircleArrow, CircleArrowThemeEnum } from "../CircleArrow";
 
 export interface HomeProps {
@@ -48,10 +50,9 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
         <h2 className="text-2xl font-bold">Recently Posted</h2>
         <div className="mt-8 grid grid-cols-12 gap-8 grid-rows-2">
           <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
-            {/* <img className="rounded-2xl" src={recentlyPosts[0].frontmatter.cover} alt="" /> */}
-            <div className="rounded-2xl bg-gray-300"></div>
+            <img className="rounded-2xl" src={Scene} alt="" />
             <div>
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">解决方案</span>
+              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">{recentlyPosts[0].frontmatter.categories}</span>
               <a
                 href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(recentlyPosts[0].frontmatter)}`}
                 className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]">
@@ -63,9 +64,9 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
             </div>
           </div>
           <div className="relative sm:col-span-12 xl:col-span-6 grid gap-3 grid-cols-12">
-            <div className="rounded-2xl col-span-5 bg-slate-300"></div>
+            <img className="rounded-2xl col-span-5" src={Steps} alt="" />
             <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">解决方案</span>
+              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">{recentlyPosts[1].frontmatter.categories}</span>
               <a
                 href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(recentlyPosts[1].frontmatter)}`}
                 className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]">
@@ -79,9 +80,9 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
             </div>
           </div>
           <div className="relative sm:col-span-12 xl:col-span-6 grid gap-3 grid-cols-12">
-            <div className="rounded-2xl col-span-5 bg-slate-300"></div>
+            <img className="rounded-2xl col-span-5" src={Waves} alt="" />
             <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">解决方案</span>
+              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">{recentlyPosts[2].frontmatter.categories}</span>
               <a
                 href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(recentlyPosts[2].frontmatter)}`}
                 className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]">
