415e6b3de3393655c131cf521ad81ff25010f936
zhanglun <zhanglun1410@gmail.com>
2023-02-21 20:16:27 +0800

    fix: fix dark mode

--- src/components/ArchiveList.tsx
+++ src/components/ArchiveList.tsx
@@ -16,14 +16,15 @@ export const ArchiveList = ({
             mt-0 min-w-full pointer-events-none 
             absolute translate-x-[-50%] left-[50%]
             text-[#e9edf1] text-center select-none 
-            // drop-shadow-[2px_2px_0_hsl(210deg_1%_69%_/_56%)]
+            drop-shadow-[2px_2px_0_hsl(210deg_1%_69%_/_56%)]
             ">
             {year}
           </div>
         <ul key={year} className="relative grid w-full lg:gap-y-14 lg:grid-cols-[repeat(12,1fr)] lg:gap-6">
-          {archiveMap.get(year)?.map(({ frontmatter, compiledContent }) => {
+          {archiveMap.get(year)?.map(({ frontmatter, compiledContent }, idx) => {
             return (
               <Card
+                key={idx}
                 href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(frontmatter)}`}
                 frontmatter={frontmatter}
                 rawContent={compiledContent()}
