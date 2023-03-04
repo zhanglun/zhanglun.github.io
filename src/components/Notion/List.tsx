a9342b85e56f34d67a1898ca064d3c2944a0df5c
zhanglun <zhanglun1410@gmail.com>
2023-03-04 20:21:23 +0800

    notion folder

--- src/components/Notion/List.tsx
+++ src/components/Notion/List.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import { NotionRenderer } from 'react-notion-x'
-import { Code } from 'react-notion-x/build/third-party/code'
+// import { Code } from 'react-notion-x/build/third-party/code'
 import { Collection } from 'react-notion-x/build/third-party/collection'
-import { Equation } from 'react-notion-x/build/third-party/equation'
-import { Modal } from 'react-notion-x/build/third-party/modal'
-import { Pdf } from 'react-notion-x/build/third-party/pdf'
+// import { Equation } from 'react-notion-x/build/third-party/equation'
+// import { Modal } from 'react-notion-x/build/third-party/modal'
+// import { Pdf } from 'react-notion-x/build/third-party/pdf'
 
 export const NotionList =  ({ recordMap }: any) => (
   <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
