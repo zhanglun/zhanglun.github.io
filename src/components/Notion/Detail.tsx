870d6702529b919d565f906fbac7b79f12256020
zhanglun <zhanglun1410@gmail.com>
2023-03-06 17:06:39 +0800

    fix: fix astro routing pages

--- src/components/Notion/Detail.tsx
+++ src/components/Notion/Detail.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react'
 import { NotionRenderer } from 'react-notion-x'
 import { Code } from 'react-notion-x/build/third-party/code'
-import * as test from 'react-notion-x/build/third-party/code.js'
 import { Collection } from 'react-notion-x/build/third-party/collection'
 import { Equation } from 'react-notion-x/build/third-party/equation'
 import { Modal } from 'react-notion-x/build/third-party/modal'
 import { Pdf } from 'react-notion-x/build/third-party/pdf'
-
-
-console.log('Code ===>', test);
 
 export const NotionDetail = ({ recordMap }: any) => {
   return <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
