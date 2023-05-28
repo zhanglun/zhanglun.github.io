7e733d862fba3dbac144142d11322d6b2441d24a
zhanglun <zhanglun1410@gmail.com>
2023-05-28 09:41:38 +0800

    feat: add banner svg

--- src/components/Home/Rich.tsx
+++ src/components/Home/Rich.tsx
@@ -6,6 +6,7 @@ import Icon from "../../assets/icon.png";
 import Scene from "../../assets/blob-scene-haikei.svg";
 import Steps from "../../assets/layered-steps-haikei.svg";
 import Waves from "../../assets/layered-waves-haikei.svg";
+import { Colorful } from "./Colorful";
 
 export interface HomeProps {
   siteInfo: any;
@@ -15,7 +16,15 @@ export interface HomeProps {
 
 export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
   return (
-    <div className="max-w-screen-2xl context m-auto h-[calc(100vh-152px)] grid items-center justify-center">
+    <div className="max-w-screen-2xl context m-auto grid items-center justify-center">
+      <div
+        className="m-0 min-w-0 absolute left-[calc((100%_-_1882px)_/_2)] top-[-200px] right-0 z-[-1] bg-white
+    overflow-hidden"
+      >
+        <div className="m-0 min-w-0 select-none pointer-events-none max-h-[calc(100vh_+_200px)]">
+          <Colorful />
+        </div>
+      </div>
       <div className="grid items-center justify-center pt-24 pb-8">
         <div className="grid gap-10 grid-rows-1">
           <div className="grid gap-8 items-center grid-rows-[1fr_1fr] sm:grid-cols-[9rem_1fr] sm:grid-rows-1">
@@ -33,25 +42,37 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
           <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
             {/* <img className="rounded-2xl" src={Scene} alt="" /> */}
             <div>
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">{recentlyPosts[0].frontmatter.categories}</span>
+              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
+                {recentlyPosts[0].frontmatter.categories}
+              </span>
               <a
-                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(recentlyPosts[0].frontmatter)}`}
-                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]">
+                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
+                  recentlyPosts[0].frontmatter
+                )}`}
+                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
+              >
                 <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
                   {recentlyPosts[0].frontmatter.title}
                 </h2>
               </a>
-              <p className="break-all text-sm leading-tight tracking-tighter text-gray-500">{recentlyPosts[0].frontmatter.description}</p>
+              <p className="break-all text-sm leading-tight tracking-tighter text-gray-500">
+                {recentlyPosts[0].frontmatter.description}
+              </p>
             </div>
           </div>
           {/* <div className="relative sm:col-span-12 xl:col-span-6 grid gap-3 grid-cols-12"> */}
           <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
             {/* <img className="rounded-2xl col-span-5" src={Steps} alt="" /> */}
             <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">{recentlyPosts[1].frontmatter.categories}</span>
+              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
+                {recentlyPosts[1].frontmatter.categories}
+              </span>
               <a
-                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(recentlyPosts[1].frontmatter)}`}
-                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]">
+                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
+                  recentlyPosts[1].frontmatter
+                )}`}
+                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
+              >
                 <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
                   {recentlyPosts[1].frontmatter.title}
                 </h2>
@@ -65,10 +86,15 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
           <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
             {/* <img className="rounded-2xl col-span-5" src={Waves} alt="" /> */}
             <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">{recentlyPosts[2].frontmatter.categories}</span>
+              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
+                {recentlyPosts[2].frontmatter.categories}
+              </span>
               <a
-                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(recentlyPosts[2].frontmatter)}`}
-                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]">
+                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
+                  recentlyPosts[2].frontmatter
+                )}`}
+                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
+              >
                 <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
                   {recentlyPosts[2].frontmatter.title}
                 </h2>
@@ -80,10 +106,15 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
           </div>
           <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
             <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">{recentlyPosts[2].frontmatter.categories}</span>
+              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
+                {recentlyPosts[2].frontmatter.categories}
+              </span>
               <a
-                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(recentlyPosts[3].frontmatter)}`}
-                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]">
+                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
+                  recentlyPosts[3].frontmatter
+                )}`}
+                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
+              >
                 <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
                   {recentlyPosts[3].frontmatter.title}
                 </h2>
@@ -95,6 +126,6 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
