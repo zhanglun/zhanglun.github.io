1dcfaee2c41e493cf0e170b59b650581fcdf5d42
zhanglun <zhanglun1410@gmail.com>
2023-06-06 17:34:21 +0800

    feat: update header

---
+++ src/components/Notion/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { ExtendedRecordMap } from 'notion-types'
+import { getPageTitle } from 'notion-utils'
+import { NotionRenderer } from 'react-notion-x'
+
+export const NotionPage = ({
+  recordMap,
+  rootPageId
+}: {
+  recordMap: ExtendedRecordMap
+  rootPageId?: string
+}) => {
+  if (!recordMap) {
+    return null
+  }
+
+  const title = getPageTitle(recordMap)
+  console.log(title, recordMap)
+
+  return (
+    <>
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage={true}
+        darkMode={false}
+        rootPageId={rootPageId}
+      />
+    </>
+  )
+}
