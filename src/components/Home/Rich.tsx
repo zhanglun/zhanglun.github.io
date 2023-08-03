aad2bb8c7e8e4d1946b4380995871f241ca5067f
zhanglun <zhanglun1410@gmail.com>
2023-08-03 14:27:57 +0800

    improvement: replace svg icon

--- src/components/Home/Rich.tsx
+++ src/components/Home/Rich.tsx
@@ -32,91 +32,27 @@ export const RichHome = ({ siteInfo, menu, recentlyPosts }: HomeProps) => {
       <div className="my-24">
         <h2 className="text-2xl font-bold">Recently Posted</h2>
         <div className="mt-8 grid gap-8 grid-rows-3 sm:grid-cols-12 sm:grid-rows-2">
-          <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
-            {/* <img className="rounded-2xl" src={Scene} alt="" /> */}
+          {recentlyPosts.map((post) => (<div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
             <div>
               <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
-                {recentlyPosts[0].frontmatter.categories}
+                {post.frontmatter.categories}
               </span>
               <a
                 href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
-                  recentlyPosts[0].frontmatter
+                  post.frontmatter
                 )}`}
                 className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
               >
                 <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
-                  {recentlyPosts[0].frontmatter.title}
+                  {post.frontmatter.title}
                 </h2>
               </a>
               <p className="break-all text-sm leading-tight tracking-tighter text-gray-500">
-                {recentlyPosts[0].frontmatter.description}
+                {post.frontmatter.description}
               </p>
             </div>
-          </div>
-          {/* <div className="relative sm:col-span-12 xl:col-span-6 grid gap-3 grid-cols-12"> */}
-          <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
-            {/* <img className="rounded-2xl col-span-5" src={Steps} alt="" /> */}
-            <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
-                {recentlyPosts[1].frontmatter.categories}
-              </span>
-              <a
-                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
-                  recentlyPosts[1].frontmatter
-                )}`}
-                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
-              >
-                <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
-                  {recentlyPosts[1].frontmatter.title}
-                </h2>
-              </a>
-              <p className="break-all text-sm leading-tight tracking-tighter text-gray-500">
-                {recentlyPosts[1].frontmatter.description}
-              </p>
-            </div>
-          </div>
-          {/* <div className="relative sm:col-span-12 xl:col-span-6 grid gap-3 grid-cols-12"> */}
-          <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
-            {/* <img className="rounded-2xl col-span-5" src={Waves} alt="" /> */}
-            <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
-                {recentlyPosts[2].frontmatter.categories}
-              </span>
-              <a
-                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
-                  recentlyPosts[2].frontmatter
-                )}`}
-                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
-              >
-                <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
-                  {recentlyPosts[2].frontmatter.title}
-                </h2>
-              </a>
-              <p className="break-all text-sm leading-tight tracking-tighter text-gray-500">
-                {recentlyPosts[2].frontmatter.description}
-              </p>
-            </div>
-          </div>
-          <div className="grid gap-3 sm:col-span-12 xl:col-span-6 row-span-2">
-            <div className="col-span-7">
-              <span className="before:content-[' '] flex items-center text-[0.875rem] font-normal before:mr-1 before:block before:h-2 before:w-2 before:rounded-2xl before:bg-[currentColor]">
-                {recentlyPosts[2].frontmatter.categories}
-              </span>
-              <a
-                href={`${import.meta.env.BASE_URL.slice(0, -1)}/blogs/${slugify(
-                  recentlyPosts[3].frontmatter
-                )}`}
-                className="block pt-3 pb-4 text-[1.125rem] font-semibold text-[var(--color-text-base)]"
-              >
-                <h2 className="text-2xl font-medium leading-none tracking-normal decoration-dashed hover:underline">
-                  {recentlyPosts[3].frontmatter.title}
-                </h2>
-              </a>
-              <p className="break-all text-sm leading-tight tracking-tighter text-gray-500">
-                {recentlyPosts[3].frontmatter.description}
-              </p>
-            </div>
-          </div>
+          </div>)
+          )}
         </div>
       </div>
     </div>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import Card from "@components/Card";
 import slugify from "@utils/slugify";
 import type { Frontmatter } from "src/types";
+import { Search } from "lucide-react";
 
 type SearchItem = {
   title: string;
@@ -73,12 +74,10 @@ export default function SearchBar({ searchList }: Props) {
   }, [inputVal]);
 
   return (
-    <>
+    <div className="max-w-5xl m-auto">
       <label className="relative block">
         <span className="absolute inset-y-0 left-0 flex items-center pl-2 opacity-75">
-          <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"></path>
-          </svg>
+          <Search />
         </span>
         <input
           className="placeholder:italic placeholder:text-opacity-75 py-3 pl-10 pr-3 
@@ -117,6 +116,6 @@ export default function SearchBar({ searchList }: Props) {
             />
           ))}
       </ul>
-    </>
+    </div>
   );
 }
