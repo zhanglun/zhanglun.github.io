2d867c2fe645cfd0eeb1dce64d77efb797eec8d7
zhanglun <zhanglun1410@gmail.com>
2023-11-16 17:49:43 +0800

    fix: fix og images

--- src/pages/[ogTitle].svg.ts
+++ src/pages/[ogTitle].svg.ts
@@ -2,9 +2,16 @@ import type { APIRoute } from "astro";
 import { getCollection } from 'astro:content';
 import generateOgImage from "@utils/generateOgImage";
 
-export const GET: APIRoute = async ({ params }) => ({
-  body: await generateOgImage(params.ogTitle),
-});
+export const GET: APIRoute = async ({ params }) => {
+  const svg = await generateOgImage(params.ogTitle);
+
+  return new Response(svg, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/svg+xml",
+    },
+  });
+};
 
 
 const postImportResult = await getCollection("blogs", ({ data }) => !data.draft);
