0de587623dfb73999c56652341122d10f0d84a68
zhanglun <zhanglun1410@gmail.com>
2024-05-31 13:56:05 +0800

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig} from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import path from "node:path";
-import { camelCase } from "change-case";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,9 +17,9 @@ export default defineConfig({
     react(),
     sitemap(),
   ],
-  image: {
-    service: passthroughImageService(),
-  },
+  // image: {
+  //   service: squooshImageService(),
+  // },
   markdown: {
     remarkPlugins: [
       remarkToc,
