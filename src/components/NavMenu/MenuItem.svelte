83483af83176804ac0616f8cff31016d1a82964c
zhanglun <zhanglun1410@gmail.com>
2024-10-17 13:52:13 +0800

    feat: dark mode switch

--- src/components/NavMenu/MenuItem.svelte
+++ src/components/NavMenu/MenuItem.svelte
@@ -1,3 +1,11 @@
-<div class="p-2 rounded-full hover:bg-gray-300">
+<script lang="ts">
+  export let onClick: Function = null;
+  export let className: Stirng = "";
+</script>
+
+<div
+  class={`${className} p-2 rounded-full hover:bg-gray-300`}
+  on:click={onClick}
+>
   <slot />
 </div>
