e81f9d4fe56cbcd2a12c1f70283ba1b87d57df6b
zhanglun <zhanglun1410@gmail.com>
2024-11-14 10:38:53 +0800

    fix: styles

--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -17,9 +17,14 @@
 
 <button
   class={clsx(
-    "px-2.5 py-1 rounded-lg text-xs text-orange-600 font-medium bg-orange-50/80 hover:bg-orange-100 capitalize flex items-center gap-2 border border-orange-200",
+    "px-2.5 py-1 rounded-lg text-xsfont-medium  capitalize flex items-center gap-2 border ",
     {
-      "bg-orange-600 text-orange-100 border-orange-600 hover:text-orange-100 hover:bg-orange-600": selected,
+      "text-orange-600 bg-orange-50/80 hover:bg-orange-100 border-orange-200":
+        !selected,
+    },
+    {
+      "bg-orange-600 text-white border-orange-600 hover:text-orange-100 hover:bg-orange-600":
+        selected,
     }
   )}
   onclick={handleClick}
