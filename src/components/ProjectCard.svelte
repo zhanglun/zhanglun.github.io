89ef548351cc1f41d70f6fe9ab3459b03eb7f337
zhanglun <zhanglun1410@gmail.com>
2025-02-14 14:34:05 +0800

    feat: projects

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -16,9 +16,7 @@
 </script>
 
 <!-- Card -->
-<div
-  class="item h-full flex flex-col overflow-hidden border"
->
+<div class="item h-full flex flex-col overflow-hidden">
   <a href={href || "#"} class="block cursor-pointer col-start-1 col-end-7">
     {#if video}
       <video
@@ -37,31 +35,26 @@
     {/if}
   </a>
   <!-- Card Header -->
-  <div class="px-2 flex flex-col col-start-7 col-end-12 row-span-1">
+  <div class="content px-2 flex flex-col col-start-7 col-end-12 row-span-1">
     <div class="space-y-1">
       <!-- Card Title -->
-      <div class="mt-1 text-base">{title}</div>
-      <time class="font-sans text-xs">{dates}</time>
+      <div class="title mt-1 text-2xl">{title}</div>
+      <time class="font-sans">{dates}</time>
       <div class="hidden font-sans text-xs underline print:visible">
         {link?.replace("https://", "").replace("www.", "").replace("/", "")}
       </div>
       <div
-        class="prose dark:prose-invert max-w-full text-pretty font-sans text-xs text-muted-foreground"
+        class="description prose dark:prose-invert max-w-full text-pretty font-sans text-xs text-muted-foreground"
       >
         {@html marked(description)}
       </div>
     </div>
     <!-- Card Content -->
-    <div
-      class="mt-auto flex flex-col px-2 text-pretty font-sans text-sm text-muted-foreground"
-    >
+    <div class="mt-auto flex flex-col">
       {#if tags && tags.length > 0}
         <div class="mt-2 flex flex-wrap gap-1">
           {#each tags as tag}
-            <Badge
-              class="rounded-[4px] px-1 py-0 text-[10px]"
-              variant="secondary"
-            >
+            <Badge class="text-smallcaps badge">
               {tag}
             </Badge>
           {/each}
@@ -69,7 +62,7 @@
       {/if}
     </div>
     <!-- Card Footer -->
-    <div class="px-2 pb-2 flex items-center pt-2">
+    <div class="pb-2 flex items-center pt-2">
       {#if links && links.length > 0}
         <div class="flex flex-row flex-wrap items-start gap-1">
           {#each links as link}
@@ -104,5 +97,26 @@
     grid-gap: 20px;
     gap: 20px;
     --overlap: calc(3px + 0.3vw);
+  }
+
+  .content:hover .title {
+    background-image: linear-gradient(
+      transparent var(--overlap),
+      var(--highlightColor) var(--overlap)
+    );
+    color: var(--invertedTextColor);
+  }
+
+  .content:hover .description {
+    background-image: linear-gradient(
+      transparent calc(var(--overlap) * 0.6),
+      var(--highlightColor) calc(var(--overlap) * 0.6)
+    );
+    color: var(--invertedTextColor);
+  }
+  .content:hover .badge {
+    background: var(--highlightColor);
+    color: var(--invertedTextColor);
+    border-color: var(--invertedTextColor);
   }
 </style>
