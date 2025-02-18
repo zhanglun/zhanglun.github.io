f43ce4c4d1d8042d691689efbe4c52f241dd0286
zhanglun <zhanglun1410@gmail.com>
2025-02-18 14:51:34 +0800

    feat: clear filter

--- src/components/DirectoryToggle.svelte
+++ src/components/DirectoryToggle.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-  const { className, name, list, onFilter} = $props();
+  const { className, name, list, onFilter } = $props();
 
-  let renderData: [string, number, boolean][] = $state(list.map((_) => [..._, false]))
+  let renderData: [string, number, boolean][] = $state(
+    list.map(_ => [..._, false])
+  );
 
-  function handleSelect (item: [string, number, boolean], idx: number) {
-    renderData[idx][2] = !renderData[idx][2]
-    
-    onFilter(renderData.filter((_) => _[2]))
+  function handleSelect(item: [string, number, boolean], idx: number) {
+    renderData[idx][2] = !renderData[idx][2];
+
+    onFilter(renderData.filter(_ => _[2]));
+  }
+
+  export function clear() {
+    renderData = renderData.map(_ => {
+      _[2] = false;
+      return _;
+    });
   }
 </script>
 
@@ -47,40 +56,43 @@
       <ul class="filterList">
         {#each renderData as item, index}
           <li class="filterList__item">
-            <div class="filterList__option" onclick={() => handleSelect(item, index)}>
+            <div
+              class="filterList__option"
+              onclick={() => handleSelect(item, index)}
+            >
               {#if !item[2]}
-              <svg
-                width="10"
-                height="9"
-                viewBox="0 0 10 9"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="checkboxIcon"
-                class:checkboxIcon--active={item[2]}
-                ><title>Checkbox icon</title><path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M1 1L-3.49691e-07 1L-4.37113e-08 8L1 8L1 9L9 9L9 8L10 8L10 1L9 1L9 -3.93402e-07L1 -4.37114e-08L1 1ZM1 1L9 1L9 8L1 8L1 1Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
+                <svg
+                  width="10"
+                  height="9"
+                  viewBox="0 0 10 9"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="checkboxIcon"
+                  class:checkboxIcon--active={item[2]}
+                  ><title>Checkbox icon</title><path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M1 1L-3.49691e-07 1L-4.37113e-08 8L1 8L1 9L9 9L9 8L10 8L10 1L9 1L9 -3.93402e-07L1 -4.37114e-08L1 1ZM1 1L9 1L9 8L1 8L1 1Z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
               {/if}
               {#if item[2]}
-              <svg
-                width="10"
-                height="9"
-                viewBox="0 0 10 9"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                class="checkboxIcon"
-                class:checkboxIcon--active={item[2]}
-                ><title>Checkbox icon</title><path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M1 1L-3.49691e-07 1L-4.37113e-08 8L1 8L1 9L9 9L9 8L10 8L10 1L9 1L9 -3.93402e-07L1 -4.37114e-08L1 1ZM1 1L9 1L9 8L1 8L1 1ZM7 2L8 2L8 3L7 3L7 2ZM6 4L6 3L7 3L7 4L6 4ZM5 5L5 4L6 4L6 5L5 5ZM4 6L4 5L5 5L5 6L4 6ZM3 6L4 6L4 7L3 7L3 6ZM3 6L2 6L2 5L3 5L3 6Z"
-                  fill="currentColor"
-                ></path></svg
-              >
+                <svg
+                  width="10"
+                  height="9"
+                  viewBox="0 0 10 9"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="checkboxIcon"
+                  class:checkboxIcon--active={item[2]}
+                  ><title>Checkbox icon</title><path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M1 1L-3.49691e-07 1L-4.37113e-08 8L1 8L1 9L9 9L9 8L10 8L10 1L9 1L9 -3.93402e-07L1 -4.37114e-08L1 1ZM1 1L9 1L9 8L1 8L1 1ZM7 2L8 2L8 3L7 3L7 2ZM6 4L6 3L7 3L7 4L6 4ZM5 5L5 4L6 4L6 5L5 5ZM4 6L4 5L5 5L5 6L4 6ZM3 6L4 6L4 7L3 7L3 6ZM3 6L2 6L2 5L3 5L3 6Z"
+                    fill="currentColor"
+                  ></path></svg
+                >
               {/if}
               <span class:filterList__option--active={item[2]}>
                 {item[0]}
