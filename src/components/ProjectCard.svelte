991126ee9a04c4446a583b4cd2ea002238acb89e
zhanglun <zhanglun@yiwise.com>
2025-02-18 22:22:46 +0800

    fix: update linear bg

--- src/components/ProjectCard.svelte
+++ src/components/ProjectCard.svelte
@@ -36,17 +36,17 @@
   </a>
   <!-- Card Header -->
   <div class="content px-2 flex flex-col col-start-7 col-end-12 row-span-1">
-    <div class="space-y-1">
+    <div class="">
       <!-- Card Title -->
-      <div class="title mt-1 text-2xl">{title}</div>
-      <time class="font-sans">{dates}</time>
-      <div class="hidden font-sans text-xs underline print:visible">
+      <span class="title mt-1 mb-3 text-3xl">{title}</span>
+      <span class="hidden font-sans text-xs underline print:visible">
         {link?.replace("https://", "").replace("www.", "").replace("/", "")}
-      </div>
+      </span>
       <div
-        class="description prose dark:prose-invert max-w-full text-pretty font-sans text-xs text-muted-foreground"
+        class="mt-4 mb-4"
       >
-        {@html marked(description)}
+        <!-- {@html marked(description)} -->
+         <span class="description">{description}</span>
       </div>
     </div>
     <!-- Card Content -->
@@ -54,7 +54,7 @@
       {#if tags && tags.length > 0}
         <div class="mt-2 flex flex-wrap gap-1">
           {#each tags as tag}
-            <Badge class="badge">
+            <Badge>
               {tag}
             </Badge>
           {/each}
@@ -68,13 +68,13 @@
           {#each links as link}
             <a href={link?.href} target="_blank">
               <Badge
-                class="flex gap-1 px-2 py-1 text-[10px] items-center justify-center"
+                class="flex gap-1 items-center justify-center"
               >
                 <!-- {link.icon} -->
                 <svelte:component
                   this={link.icon}
                   class="mb-px"
-                  size={12}
+                  size={10}
                   strokeWidth={1.5}
                 />
                 {link.type}
@@ -96,7 +96,8 @@
     position: relative;
     grid-gap: 20px;
     gap: 20px;
-    --overlap: calc(3px + 0.3vw);
+    /* --overlap: calc(3px + 0.3vw); */
+    --overlap: 0px;
   }
 
   .image {
@@ -118,13 +119,13 @@
 
   .content:hover .description {
     background-image: linear-gradient(
-      transparent calc(var(--overlap) * 0.6),
-      var(--highlightColor) calc(var(--overlap) * 0.6)
+      transparent calc(var(--overlap) * 0.5),
+      var(--highlightColor) calc(var(--overlap) * 0.5)
     );
     color: var(--invertedTextColor);
   }
 
-  .content:hover .badge {
+  .content:hover :global(.badge) {
     background: var(--highlightColor);
     color: var(--invertedTextColor);
     border-color: var(--invertedTextColor);
